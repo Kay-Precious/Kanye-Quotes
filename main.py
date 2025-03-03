7cb07a0aeaa33114fb7774a4dd7667b1aaa014ee
Kay Precious <kayprecious977@gmail.com>
2025-03-03 19:16:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ background_img = PhotoImage(
     file="./background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=(
-    "Arial", 30, "bold"), fill="white")
+    "Arial", 20, "bold"), fill="black")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(
